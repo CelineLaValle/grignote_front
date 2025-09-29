c2a3213d0907cc2e8132c7f6d3536179925cd83e
Céline <lavalleceline@yahoo.com>
2025-09-29 22:54:12 +0200

    stable local

--- src/components/AccueilPage.jsx
+++ src/components/AccueilPage.jsx
@@ -27,7 +27,7 @@ function AccueilPage() {
         throw new Error(`Erreur lors de la récupération des données: ${response.status}`);
       }
 
-      const data = await response.text();
+      const data = await response.json();
       console.log(data);
       
       
